62dd2ff3de2965ce25a5db2aa1ffb68591bffc9b
279437541 <78850005+279437541@users.noreply.github.com>
2021-05-21 23:01:54 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-ZZZ="package/lean/default-settings/files/zzz-default-settings"
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-sed -i "/uci commit fstab/a\uci commit network" $ZZZ
-sed -i "/uci commit network/i\uci set network.lan.ipaddr='10.10.10.251'" $ZZZ               # IPv4 地址(openwrt后台地址)
-sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ             # IPv4 子网掩码
-sed -i "/uci commit network/i\uci set network.lan.gateway='10.10.10.250'" $ZZZ              # IPv4 网关
-sed -i "/uci commit network/i\uci set network.lan.broadcast='10.10.10.255'" $ZZZ            # IPv4 广播
-sed -i "/uci commit network/i\uci set network.lan.dns='10.10.10.253'" $ZZZ                  # DNS(多个DNS要用空格分开)
-sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                        # 去掉LAN口使用内置的 IPv6 管理
-echo "close_dhcp" > package/base-files/files/etc/closedhcp                                  # 关闭DHCP服务
 
-sed -i 's/luci-theme-bootstrap/luci-theme-rosy/g' feeds/luci/collections/luci/Makefile      # 强制选择rosy为默认主题选项
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'" $ZZZ             # 修改主机名称为OpenWrt
 
-sed -i "s/OpenWrt /MCydia Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ       # 增加自己个性名称MCydia
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-DaHuiLang'' package/lean/default-settings/files/zzz-default-settings
 
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                      # 设置密码为空
 
-# sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile                       # 修改内核版本为4.19
+sed -i "s/OpenWrt /大灰狼 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+
+sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' feeds/luci/collections/luci/Makefile
+
+
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 
 # 修改插件名字
